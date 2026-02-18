8e100c70d98576759d992cfd39f982be58d1a193
Bhumi <bbhumi576@gmail.com>
2026-02-18 10:11:56 +0530

    Create Print_boundaryElements.java

---
+++ Print_boundaryElements.java
@@ -0,0 +1,26 @@
+public class multiArray2 {
+    public static void main(String[] args) {
+
+// int [][]arr={
+//     {1,2,3},
+//     {4,5,6},
+//     {7,8,9}
+// };
+
+// int rows=arr.length;
+// int cols=arr[0].length;
+
+// for(int i=0;i<arr.length;i++){
+//     for(int j=0;j<arr[i].length;j++){
+//         if(i==0 || i==rows-1 || j==0 || j==cols-1){
+//             System.out.print(arr[i][j]+ " ");
+//         }
+//     }
+// }
+
+
+// output-->>
+// 1 2 3 4 6 7 8 9 
+
+    }
+}
